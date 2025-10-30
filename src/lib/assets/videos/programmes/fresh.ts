2396ca29da04ea205e4b3ba90c102275840196fe
rubenesque <a.ruben00001@gmail.com>
2025-10-30 12:29:17 +0000

    mime video error fix

--- src/lib/assets/videos/programmes/fresh.ts
+++ src/lib/assets/videos/programmes/fresh.ts
@@ -1,4 +1,4 @@
-import intro from '^files/videos/programmes/fresh/intro.mp4';
-import participant_testimonial from '^files/videos/programmes/fresh/participant-testimonial.mp4';
+import intro from '^files/videos/programmes/fresh/intro.mp4?url';
+import participant_testimonial from '^files/videos/programmes/fresh/participant-testimonial.mp4?url';
 
 export { intro, participant_testimonial };
