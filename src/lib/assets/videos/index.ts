8164e940a819f834efaba7cd480c22ee86102878
rubenesque <a.ruben00001@gmail.com>
2025-10-30 12:43:13 +0000

    ...

--- src/lib/assets/videos/index.ts
+++ src/lib/assets/videos/index.ts
@@ -1,4 +1,4 @@
-import our_mission from '^files/videos/our-mission.mp4';
-import banner from '^files/videos/banner.mp4';
+import banner from '../_files/videos/banner.mp4';
+import our_mission from '../_files/videos/our-mission.mp4';
 
 export { banner, our_mission };
